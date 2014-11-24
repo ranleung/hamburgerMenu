5cc16d8821df2b6b6315494f3d47d3f01387a2aa
Randall Leung <leungran@gmail.com>
2014-11-24 12:50:26 -0800

    inital commit

---
+++ hamburgerMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hamburgerMenu
+//
+//  Created by Randall Leung on 11/24/14.
+//  Copyright (c) 2014 Randall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
