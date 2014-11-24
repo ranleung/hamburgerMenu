f23ba468c25a96048d60aa3623c5df9c78339482
Randall Leung <leungran@gmail.com>
2014-11-24 13:28:03 -0800

    add burger menu

--- hamburgerMenu/ViewController.m
+++ hamburgerMenu/ViewController.m
@@ -10,18 +10,47 @@
 
 @interface ViewController ()
 
+@property (retain, nonatomic) IBOutlet UIButton *burgerButton;
+@property (strong, nonatomic) UIViewController *currentVC;
+@property (strong, nonatomic) UIViewController *burgerVC;
+@property BOOL isActiveBurger;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //Create the burgerVC
+    self.burgerVC = [self.storyboard instantiateViewControllerWithIdentifier:@"BURGER_VC"];
+    [self addChildViewController:self.burgerVC];
+    self.burgerVC.view.frame = self.view.frame;
+    [self.view insertSubview:self.burgerVC.view belowSubview:self.burgerButton];
+    [self.burgerVC didMoveToParentViewController:self];
+    
+    self.isActiveBurger = NO;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)burgerDidPress:(id)sender {
+    if (self.isActiveBurger == NO) {
+        [UIView animateWithDuration:0.4 animations:^{
+            self.burgerVC.view.frame = CGRectMake(self.view.frame.size.width * 0.8, 0, self.view.frame.size.width, self.view.frame.size.height);
+        }];
+        self.isActiveBurger = YES;
+    } else {
+        [UIView animateWithDuration:0.4 animations:^{
+            self.burgerVC.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+        }];
+        self.isActiveBurger = NO;
+    }
 }
 
+
+- (void)dealloc {
+    [_burgerButton release];
+    [_currentVC release];
+    [_burgerVC release];
+    [super dealloc];
+}
 @end
